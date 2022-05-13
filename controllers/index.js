a6a9672bf7a1f06f1404ca44db856b43e165c73d
sergii-onyx <twilight.cympak@gmail.com>
2022-05-13 18:41:03 -0400

    added posts, comments, dashboard, login/out, auth etc

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api/');
 const homeRoutes = require('./home-routes.js');
+const dashboardRoutes = require('./dashboard-routes.js');
+const profileRoutes = require('./profile-routes.js');
 
 router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
+router.use('/profile', profileRoutes);
 
 module.exports = router;
--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const bcrypt = require('bcrypt');
-const sequelize = require('../config/config');
+const sequelize = require('../config/connection');
 
 // create our User model
 class User extends Model {
@@ -10,6 +10,7 @@ class User extends Model {
   }
 }
 
+// create fields/columns for User model
 User.init(
   {
     id: {
@@ -22,13 +23,24 @@ User.init(
       type: DataTypes.STRING,
       allowNull: false
     },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
     password: {
       type: DataTypes.STRING,
       allowNull: false,
       validate: {
         len: [4]
       }
-    }
+    },
+      avatar_url: {
+        type: DataTypes.STRING
+      }
   },
   {
     hooks: {
@@ -47,7 +59,7 @@ User.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'User'
+    modelName: 'user'
   }
 );
 
