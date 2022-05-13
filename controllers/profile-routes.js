e74c52a13717c83fca3b205ad32ff66fc52b606e
sergii-onyx <twilight.cympak@gmail.com>
2022-05-13 19:50:03 -0400

    added user profile api with json file to respond

--- controllers/profile-routes.js
+++ controllers/profile-routes.js
@@ -3,44 +3,20 @@ const sequelize = require("../config/connection");
 const { Post, User, Comment, Vote } = require("../models");
 const withAuth = require("../utils/auth");
 
-// get all posts for dashboard
-router.get("/", withAuth, (req, res) => {
-  console.log(req.session);
-  console.log("======================");
-  Post.findAll({
+// get user info
+router.get("/", (req, res) => {
+  User.findOne({
+    attributes: { exclude: ["password"] },
     where: {
-      user_id: req.session.user_id,
-    },
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
+      id: req.session.user_id,
+    }
   })
-    .then((dbPostData) => {
-      const posts = dbPostData.map((post) => post.get({ plain: true }));
-      res.render("profile-page", { posts, loggedIn: true });
+    .then((dbUserData) => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "No user found with this id" });
+        return;
+      }
+      res.json(dbUserData);
     })
     .catch((err) => {
       console.log(err);
@@ -48,50 +24,50 @@ router.get("/", withAuth, (req, res) => {
     });
 });
 
-router.get("/edit/:id", withAuth, (req, res) => {
-  Post.findByPk(req.params.id, {
-    attributes: [
-      "id",
-      "post_url",
-      "title",
-      "created_at",
-      [
-        sequelize.literal(
-          "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
-        ),
-        "vote_count",
-      ],
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
-        include: {
-          model: User,
-          attributes: ["username"],
-        },
-      },
-      {
-        model: User,
-        attributes: ["username"],
-      },
-    ],
-  })
-    .then((dbPostData) => {
-      if (dbPostData) {
-        const post = dbPostData.get({ plain: true });
+// router.get("/edit/:id", withAuth, (req, res) => {
+//   Post.findByPk(req.params.id, {
+//     attributes: [
+//       "id",
+//       "post_url",
+//       "title",
+//       "created_at",
+//       [
+//         sequelize.literal(
+//           "(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)"
+//         ),
+//         "vote_count",
+//       ],
+//     ],
+//     include: [
+//       {
+//         model: Comment,
+//         attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+//         include: {
+//           model: User,
+//           attributes: ["username"],
+//         },
+//       },
+//       {
+//         model: User,
+//         attributes: ["username"],
+//       },
+//     ],
+//   })
+//     .then((dbPostData) => {
+//       if (dbPostData) {
+//         const post = dbPostData.get({ plain: true });
 
-        res.render("edit-post", {
-          post,
-          loggedIn: true,
-        });
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch((err) => {
-      res.status(500).json(err);
-    });
-});
+//         res.render("edit-profile", {
+//           post,
+//           loggedIn: true,
+//         });
+//       } else {
+//         res.status(404).end();
+//       }
+//     })
+//     .catch((err) => {
+//       res.status(500).json(err);
+//     });
+// });
 
 module.exports = router;
